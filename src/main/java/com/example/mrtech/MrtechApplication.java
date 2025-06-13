d79a380fb5036b3eb95f3682324d87efd88afd22
realrishiiscool <2108120rishivardhan@gmail.com>
2025-06-13 12:07:18 +0530

    first commit

---
+++ src/main/java/com/example/mrtech/MrtechApplication.java
@@ -0,0 +1,14 @@
+package com.example.mrtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrtechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MrtechApplication.class, args);
+	}
+
+}
+	
---
+++ src/test/java/com/example/mrtech/MrtechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mrtech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MrtechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
